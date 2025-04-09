0a5313ab924d8541475c0be729a820273c9033dd
Manqoba Sibeko <manqsibeko@gmail.com>
2025-04-09 17:06:50 +0200

    inital files

---
+++ src/person_Detials.java
@@ -0,0 +1,9 @@
+public class person_Detials {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        int valueOne = Integer.MAX_VALUE;
+
+        System.out.println(valueOne);
+    }
+}
